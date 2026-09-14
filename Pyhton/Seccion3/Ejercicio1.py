c7da08b07061fba3c3b6b7cf6d7a68573011dd3a
prdesarrollo <prdesarrollo@forem.com.mx>
2026-09-14 17:06:01 -0600

    new

---
+++ Pyhton/Seccion3/Ejercicio1.py
@@ -0,0 +1,33 @@
+# Mi solucion
+'''
+print("Programa para calcular la formula general")
+
+a = float(input('Ingresa el valor "a" '))
+b = float(input('Ingresa el valor "b" '))
+c = float(input('Ingresa el valor "c" '))
+
+print(a)
+print(b)
+print(c)
+
+x1 = ( (-b) + (((b**2) - (4(a*c)))**0.5)) / (2(a))
+
+x2 = ( (-b) - (((b**2) - (4*(a*c))**0.5)) / (2(a)))
+
+print('Los resultados son "+" = {x1} y "-" ={x2}')
+'''
+#Solucion clase
+
+from math import sqrt
+
+a = float(input('Ingresa el valor "a" '))
+b = float(input('Ingresa el valor "b" '))
+c = float(input('Ingresa el valor "c" '))
+x1 = 0
+x2 = 0
+
+if ((b**2) - (4*a*c)) <0:
+    print("No se puede realizar porque no se puede scar la raiz cuadrada de un numero negativo")
+else:
+    x1 = (-b + sqrt((b**2)-(4*a*c))/(2*a))
+    x2 = (-b - sqrt((b**2)-(4*a*c))/(2*a))
---
+++ Pyhton/Seccion3/Ejercicio2.py
@@ -0,0 +1,40 @@
+'''
+Se desea tener un algoritmo que permita determinar y mostrar el promedio que ha obtenido un alumno en un determinado curso, conociendo las notas de: tres prácticas, el examen parcial y el examen final.
+
+Considere:
+
+PP = ( P1 + P2 +P3 ) / 3 PROM = ( PP + 2*EP + 3*EF ) / 6
+
+Donde: P1, P2, P3 : Practicas
+
+PP: promedio de práctica
+
+PROM: promedio
+
+EP: examen parcial
+
+EF: examen final
+
+'''
+print("Calculadora de evaluacion del alumno")
+
+P1 = float(input('Ingresa el valor de la EV1:'))
+P2 = float(input('Ingresa el valor de la EV2:'))
+P3 = float(input('Ingresa el valor de la EV3:'))
+EP = float(input('Ingresa el valor del Examen Parcial:'))
+EF = float(input('Ingresa el valor del Examen Final:'))
+
+
+PP = (P1 + P2 + P3) / 3
+
+PROM = (PP + 2*EP + 3*EF) / 6
+
+calificacion = (PP + PROM) / 2
+
+if (calificacion <= 60): 
+
+    print("Sacaste {calificacion}, no pasaste D: ")
+
+else:
+    print("Sacaste: {calificacion}, pasaste :D ")
+
