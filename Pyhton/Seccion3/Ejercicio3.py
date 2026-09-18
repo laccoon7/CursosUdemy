a22089f3f0d5723d2eb610be751dd8d43710ae4a
prdesarrollo <prdesarrollo@forem.com.mx>
2026-09-18 17:01:27 -0600

    new 18/09

--- Pyhton/Seccion3/Ejercicio3.py
+++ Pyhton/Seccion3/Ejercicio3.py
@@ -2,12 +2,11 @@
 Escribir un programa que solicite al usuario un vocal en minuscula, y luego una letra en mayúsculas. El programa debe convertir la letra en minúscula y la vocal en mayúscula, y al final, deben ser concatenadas ambas
 '''
 
-print("Coloque una vocal miniscula y una mayuscula")
+print("Coloque una vocal miniscula y una letra mayuscula")
 
-vocalMinus = input("Ingresa la vocal minuscula:")
+vocalMinus = input("Ingresa la vocal MINUSCULA:")
 
-letraMayus = input("Ingresa la vocal mayuscula:")
-
+letraMayus = input("Ingresa la letra en MAYUSCULA:")
 
 if (vocalMinus in "aeiou") and (letraMayus.isupper()):
     print("Tu vocal minuscula ahora es mayuscula:{}".format(vocalMinus.swapcase()))
@@ -15,3 +14,4 @@ if (vocalMinus in "aeiou") and (letraMayus.isupper()):
     print(vocalMinus.swapcase() + letraMayus.swapcase())
 else:
     print("Entrada invalida. Revisa los datos ingresados.")
+
