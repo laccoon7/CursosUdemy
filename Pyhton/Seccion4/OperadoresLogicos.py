03de36941941d8a8d9d620eb6c7260fa0faad9dd
laccon7 <jose.rangel4586@outlook.com>
2026-09-21 13:34:58 -0600

    new 21/09

--- Pyhton/Seccion4/OperadoresLogicos.py
+++ Pyhton/Seccion4/OperadoresLogicos.py
@@ -1,1 +1,13 @@
+print(10 > 2 and 9 > 100)
+
 print(10 > 2 and 9 > 1)
+
+print(10 > 2 and 23 == 23)
+
+print(99 != 99 and 23 == 23)
+
+print(88 <= 789 or 60 > 110)
+
+print(not 90 != 90)
+
+print(not 90 == 90)
