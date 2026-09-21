965969375df4413f10a7b207fe16cf9c3b5e6410
laccon7 <jose.rangel4586@outlook.com>
2026-09-21 07:45:54 -0600

    OP Comp y Logicos

---
+++ Pyhton/Seccion4/OperadoresComparacion.py
@@ -0,0 +1,13 @@
+num1 = 1000
+num2 = 500
+num3 = 1000
+cadena = "Comparaciones de Strings"
+cadena1 = "Comparacion de Strings"
+
+print("Mayor que",num1 > num2)
+print("Menor que",num1 < num2)
+print(num1 >= num3)
+print(num1 <= num3)
+print(num1 == num3)
+
+print("Strings",cadena > cadena1)
