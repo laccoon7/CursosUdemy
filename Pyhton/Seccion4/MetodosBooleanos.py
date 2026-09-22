65417d0c6b6a5c6a5e093eeaded00501ef3f158b
laccon7 <jose.rangel4586@outlook.com>
2026-09-22 00:56:20 -0600

    Metodos Booleanos

---
+++ Pyhton/Seccion4/MetodosBooleanos.py
@@ -0,0 +1,13 @@
+cadena = "Estoy mostrando los metodos booleanos para las Strings"
+cadena2 = "EWNFIHWBEHFBRWEHFBWUBF"
+cadena3 = "jenfwhbfrwehnwiuu"
+
+print(cadena.startswith("E"))
+print(cadena.startswith("e"))
+print(cadena.endswith("s"))
+
+print(cadena.isupper())
+print(cadena2.isupper())
+
+print(cadena2.islower())
+print(cadena3.islower())
