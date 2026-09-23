4427e93afdb77e5309d01420b08229f8b61d4365
laccon7 <jose.rangel4586@outlook.com>
2026-09-22 23:09:11 -0600

    new 22/09

---
+++ Pyhton/Seccion5/Ejercicio1.py
@@ -0,0 +1,10 @@
+'''
+Crear un programa que pida al usuario una letra, y si es vocal, muestre el mensaje "Es vocal". Sino, decirle al usuario que no es vocal
+'''
+
+vocal = input("Ingrese una vocal: ")
+
+if vocal.lower() in "aeiou":
+    print("ES vocal")
+else:
+    print("NO es vocal")
---
+++ Pyhton/Seccion5/Elif.py
@@ -0,0 +1,14 @@
+vocal = input("Ingresa tu vocal: ")
+
+if vocal.lower() == "a":
+    print("Esta vocal es la A")
+elif vocal.lower == "e":
+    print("Esta vocal es la E")
+elif vocal.lower() == "i":
+    print("Esta vocal es la I")
+elif vocal.lower() == "o":
+    print("Esta vocal es la O")
+elif vocal.lower() == "u":
+    print("Esta vocal es la U")
+else: 
+    print("NO es una vocal")
