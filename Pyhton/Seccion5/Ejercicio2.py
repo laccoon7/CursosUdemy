305d6054efed6067e879d7f207573a0607bf812f
laccon7 <jose.rangel4586@outlook.com>
2026-09-23 12:26:26 -0600

    ejercicio2

--- Pyhton/Seccion5/Ejercicio2.py
+++ Pyhton/Seccion5/Ejercicio2.py
@@ -2,3 +2,9 @@
 Escribir un programa que, dado un número entero, muestre su valor absoluto. Nota: para los números positivos su valor absoluto es igual al número (el valor absoluto de 52 es 52), mientras que, para los negativos, su valor absoluto es el número multiplicado por -1 (el valor absoluto de -52 es 52).
 '''
 
+numero = int(input("Ingresa tu numero: "))
+
+if numero > 0:
+    print("Tu numero absoluto es: {}".format(numero))
+else:
+    print("Tu numero absoluto es: {}".format(numero*-1))
